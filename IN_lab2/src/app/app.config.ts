6f9cb804e9c4ba4c43abcc731373679a034275fb
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-11-24 02:15:55 +0200

    Sign in form

--- IN_lab2/src/app/app.config.ts
+++ IN_lab2/src/app/app.config.ts
@@ -2,12 +2,14 @@ import { provideRouter, Routes } from "@angular/router";
 import { ApplicationConfig } from "@angular/core";
 
 import { HomeComponent } from "./home.component";
-import { AboutComponent } from "./about.component";
+import { SignInComponent } from "./authorization/sign-in/sign-in.component"
+import { PrivacyPolicyComponent } from "./privacy-policy.component";
 import { NotFoundComponent } from "./not-found.component";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "about", component: AboutComponent },
+  { path: "signin", component: SignInComponent },
+  { path: "privacy", component: PrivacyPolicyComponent },
   { path: "**", component: NotFoundComponent }
 ];
 
