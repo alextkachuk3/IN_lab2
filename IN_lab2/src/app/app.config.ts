cac831081eeb4d0dca0ec746491640548c273812
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-11-24 09:50:11 +0200

    User music list

--- IN_lab2/src/app/app.config.ts
+++ IN_lab2/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { provideRouter, Routes } from "@angular/router";
 import { ApplicationConfig } from "@angular/core";
 
 import { MainComponent } from "./main.component";
+import { UploadComponent } from './upload/upload.component'
 import { SignInComponent } from "./authorization/sign-in/sign-in.component"
 import { SignUpComponent } from "./authorization/sign-up/sign-up.component"
 import { PrivacyPolicyComponent } from "./privacy-policy.component";
@@ -12,6 +13,7 @@ const appRoutes: Routes = [
   { path: "", component: MainComponent, canActivate: [AuthGuard] },
   { path: "signin", component: SignInComponent, canActivate: [AuthGuard] },
   { path: "signup", component: SignUpComponent, canActivate: [AuthGuard] },
+  { path: "upload", component: UploadComponent, canActivate: [AuthGuard] },
   { path: "privacy", component: PrivacyPolicyComponent },
   { path: "**", component: NotFoundComponent }
 ];
--- IN_lab2/src/app/main.component.ts
+++ IN_lab2/src/app/main.component.ts
@@ -1,7 +1,33 @@
-import { Component } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { AuthorizationService } from "./authorization/authorization.service";
+import { NgFor } from "@angular/common";
+import { Music } from "./music";
 
 @Component({
   selector: "main",
-  template: "<h2>Music list</h2>"
+  imports: [NgFor],
+  standalone: true,
+  templateUrl: "./main.component.html",
+  styleUrls: ['./main.component.css'],
 })
-export class MainComponent { }
+export class MainComponent implements OnInit {
+  public musics: Music[] = [];
+  constructor(private http: HttpClient, private authService: AuthorizationService) { }
+
+  ngOnInit() {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.authService.getToken()}`,
+    });
+    
+    this.http.get<Music[]>('https://localhost:7232/Music', { headers }).subscribe(
+      (data) => {
+        data.forEach((element:any) => this.musics.push(element));
+        console.log(this.musics);
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+  }
+}
