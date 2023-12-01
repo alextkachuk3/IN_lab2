705e8fa91000a062fb0583edc13fb062c83480b1
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-12-01 11:22:09 +0200

    Fixed updating of username display after log in

--- IN_lab2/src/app/app.component.ts
+++ IN_lab2/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { NgFor, NgIf, NgOptimizedImage } from "@angular/common";
-import { RouterOutlet, RouterLink } from "@angular/router";
+import { Router, RouterOutlet, RouterLink, NavigationStart } from "@angular/router";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from "@angular/common/http";
 
@@ -16,16 +16,29 @@ import { AuthorizationService } from './authorization/authorization.service';
   styleUrls: ['./app.component.css'],
   providers: [AuthorizationService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public username: string = '';
-  constructor(public authService: AuthorizationService) {
+  constructor(public authService: AuthorizationService, private router: Router) {
     if (this.authService.getToken()) {
-      this.authService.getUsername()
-        .subscribe({
-          next: (data: any) => { this.username = data.username; console.log(data) },
-          error: error => console.log(error)
-        });
+      this.updateUsername();
     }
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationStart) {
+        console.log('Navigation started');
+        this.updateUsername();
+      }
+    });
+  }
+
+  updateUsername() {
+    this.authService.getUsername()
+      .subscribe({
+        next: (data: any) => { this.username = data.username; console.log(data) },
+        error: error => console.log(error)
+      });
   }
 
   title = 'IN_lab2';
