7e321a0e2abdee9035c84deb988c8f4f294d01ef
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-12-01 03:26:23 +0200

    Music upload

--- IN_lab2/src/app/upload/upload.component.ts
+++ IN_lab2/src/app/upload/upload.component.ts
@@ -1,20 +1,22 @@
 import { Component } from "@angular/core";
 import { Router, RouterLink } from "@angular/router";
 import { FormsModule } from "@angular/forms";
-import { HTTP_INTERCEPTORS, HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 import { AuthorizationService } from "../authorization/authorization.service";
+import { UploadService } from "./upload.service";
 
 @Component({
   selector: "upload",
   standalone: true,
   imports: [RouterLink, FormsModule],
   templateUrl: './upload.component.html',
-  providers: [AuthorizationService]
+  providers: [UploadService, AuthorizationService, FormsModule]
 })
 export class UploadComponent {
+  musicName: string = '';
   selectedFile: File | null = null;
-  constructor(private authService: AuthorizationService, private http: HttpClient, private router: Router) { }
+  constructor(private uploadService: UploadService, private authService: AuthorizationService, private http: HttpClient, private router: Router) { }
 
   onFileSelected(event: any): void {
     this.selectedFile = event.target.files[0] as File;
@@ -23,28 +25,15 @@ export class UploadComponent {
   onSubmit(): void {
     if (this.selectedFile) {
 
-      const headers = new HttpHeaders({
-        'Authorization': `Bearer ${this.authService.getToken()}`,
-      });
-
-      this.http.post("https://localhost:7232/Music/Upload", { 'name': 'testfilename', 'file': this.selectedFile, headers }).subscribe(
-        (response) => {
-          console.log('File uploaded successfully', response);
-        },
-        (error) => {
-          console.error('Error uploading file', error);
-        }
-      );
-
-
-      //this.http.post('https://localhost:7232/Music/Upload', formData).subscribe(
-      //  (response) => {
-      //    console.log('File uploaded successfully', response);
-      //  },
-      //  (error) => {
-      //    console.error('Error uploading file', error);
-      //  }
-      //);
+      this.uploadService.uploadMusic(this.selectedFile, this.musicName)
+        .subscribe(
+          response => {
+            console.log('File uploaded successfully', response);
+          },
+          error => {
+            console.error('Error uploading file', error);
+          }
+        );
     }
   }
 }
